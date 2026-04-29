58c60810511c5fbae34f3930ae57ff23e8f5238b
Supremum1 <artem.leshsuk123@gmail.com>
2026-04-29 12:01:05 +0500

    db was connected

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module hellodb
+
+go 1.26.1
+
+require github.com/lib/pq v1.12.3 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type Product struct {
+	id    int64
+	name  string
+	price int32
+}
+
+func main() {
+	db, err := sql.Open(
+		"postgres",
+		"postgres://postgres:2006@localhost:5432/products?sslmode=disable",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query("SELECT * from products")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	products := make([]*Product, 0)
+
+	for rows.Next() {
+		product := new(Product)
+		err := rows.Scan(&product.id, &product.name, &product.price)
+		if err != nil {
+			log.Fatal(err)
+		}
+		products = append(products, product)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	for _, pr := range products {
+		fmt.Printf("%d, %s, %d\n", pr.id, pr.name, pr.price)
+	}
+
+}
